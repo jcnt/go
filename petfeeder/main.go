ff815299b6eb05b9f7c389be4d6100cede09ef7b
Jacint Juhasz <yyaazz@gmail.com>
2024-11-18 23:22:46 +0100

    Lazy commit Mon Nov 18 23:22:46 CET 2024

--- petfeeder/main.go
+++ petfeeder/main.go
@@ -44,8 +44,8 @@ var cfg = mysql.Config{
 
 func main() {
 
-	loadOwners()
-	loadPets()
+	//	loadOwners()
+	//	loadPets()
 	a, b := randData()
 
 	fmt.Println(owners)
@@ -117,223 +117,29 @@ func randData() (Owner, Pet) {
 	t := time.Now()
 	rand.NewSource(t.UnixNano())
 
-	first := []string{
-		"Jack",
-		"Harry",
-		"Jacob",
-		"Charlie",
-		"Thomas",
-		"George",
-		"Oscar",
-		"James",
-		"William",
-		"Jake",
-		"Connor",
-		"Callum",
-		"Jacob",
-		"Kyle",
-		"Joe",
-		"Reece",
-		"Rhys",
-		"Charlie",
-		"Damian",
-		"Noah",
-		"Liam",
-		"Mason",
-		"Jacob",
-		"William",
-		"Ethan",
-		"Michael",
-		"Alexander",
-		"James",
-		"Daniel",
-		"James",
-		"John",
-		"Robert",
-		"Michael",
-		"William",
-		"David",
-		"Richard",
-		"Joseph",
-		"Charles",
-		"Thomas",
-		"Larry",
+	first := []string{"Jack", "Harry", "Jacob", "Charlie", "Thomas", "George", "Oscar", "James", "William", "Jake", "Connor", "Callum", "Jacob", "Kyle", "Joe", "Reece", "Rhys", "Charlie", "Damian", "Noah", "Liam", "Mason", "Jacob", "William", "Ethan", "Michael", "Alexander", "James", "Daniel", "James", "John", "Robert", "Michael", "William", "David", "Richard", "Joseph", "Charles", "Thomas", "Larry"}
+	last := []string{"Jones", "Williams", "Taylor", "Brown", "Davies", "Evans", "Wilson", "Thomas", "Johnson", "Roberts", "Robinson", "Thompson", "Wright", "Walker", "White", "Edwards", "Hughes", "Green", "Hall", "Lewis", "Harris", "Clarke", "Patel", "Jackson", "Wood", "Turner", "Martin", "Cooper", "Hill", "Ward", "Morris", "Moore", "Clark", "Lee", "King", "Baker", "Harrison", "Morgan", "Allen", "Johnson"}
+	street := []string{"Third", "First", "Fourth", "Park", "Fifth", "Main", "Sixth", "Oak", "Seventh", "Pine", "Maple", "Cedar", "Eighth", "Elm", "View", "Washington", "Ninth", "Lake", "Hill", "High", "Station", "Main", "Park", "Church", "London", "Victoria", "Green", "Manor", "Park", "Queens", "Grange", "Kings", "Kingsway", "Windsor", "Alexander", "York", "Broadway", "Springfield", "Richmond", "Taylor"}
+	city := []string{"Sun Prairie", "McFarland", "Windsor", "Monona", "Waunakee", "Madison", "Sun Prairie", "McFarland", "Windsor", "Monona", "Waunakee", "Madison", "Sun Prairie", "McFarland", "Windsor", "Monona", "Waunakee", "Madison", "Sun Prairie", "McFarland", "Windsor", "Monona", "Waunakee", "Madison", "Sun Prairie", "McFarland", "Windsor", "Monona", "Waunakee", "Madison", "Sun Prairie", "McFarland", "Windsor", "Monona", "Waunakee", "Madison", "Sun Prairie", "McFarland", "Windsor", "Monona"}
+	pet := []string{"Milo", "Teddy", "Buddy", "Alfie", "Max", "Charlie", "Bailey", "Reggie", "Bruno", "Hugo", "Luna", "Bella", "Lola", "Poppy", "Daisy", "Coco", "Ruby", "Molly", "Rosie", "Willow", "Milo", "Teddy", "Buddy", "Alfie", "Max", "Charlie", "Bailey", "Reggie", "Bruno", "Hugo", "Luna", "Bella", "Lola", "Poppy", "Daisy", "Coco", "Ruby", "Molly", "Rosie", "Willow"}
+
+	// temporary
+	id := strconv.Itoa(rand.Intn(10) + 20)
+	// temporary
+
+	var month string
+	if m := rand.Intn((12) + 1); m < 10 {
+		month = "0" + strconv.Itoa(m)
+	} else {
+		month = strconv.Itoa(m)
 	}
 
-	last := []string{
-		"JONES",
-		"WILLIAMS",
-		"TAYLOR",
-		"BROWN",
-		"DAVIES",
-		"EVANS",
-		"WILSON",
-		"THOMAS",
-		"JOHNSON",
-		"ROBERTS",
-		"ROBINSON",
-		"THOMPSON",
-		"WRIGHT",
-		"WALKER",
-		"WHITE",
-		"EDWARDS",
-		"HUGHES",
-		"GREEN",
-		"HALL",
-		"LEWIS",
-		"HARRIS",
-		"CLARKE",
-		"PATEL",
-		"JACKSON",
-		"WOOD",
-		"TURNER",
-		"MARTIN",
-		"COOPER",
-		"HILL",
-		"WARD",
-		"MORRIS",
-		"MOORE",
-		"CLARK",
-		"LEE",
-		"KING",
-		"BAKER",
-		"HARRISON",
-		"MORGAN",
-		"ALLEN",
-		"JOHNSON",
-	}
-
-	street := []string{
-		"Third",
-		"First",
-		"Fourth",
-		"Park",
-		"Fifth",
-		"Main",
-		"Sixth",
-		"Oak",
-		"Seventh",
-		"Pine",
-		"Maple",
-		"Cedar",
-		"Eighth",
-		"Elm",
-		"View",
-		"Washington",
-		"Ninth",
-		"Lake",
-		"Hill",
-		"High",
-		"Station",
-		"Main",
-		"Park",
-		"Church",
-		"London",
-		"Victoria",
-		"Green",
-		"Manor",
-		"Park",
-		"Queens",
-		"Grange",
-		"Kings",
-		"Kingsway",
-		"Windsor",
-		"Alexander",
-		"York",
-		"Broadway",
-		"Springfield",
-		"Richmond",
-		"Taylor",
-	}
-
-	city := []string{
-		"Sun Prairie",
-		"McFarland",
-		"Windsor",
-		"Monona",
-		"Waunakee",
-		"Madison",
-		"Sun Prairie",
-		"McFarland",
-		"Windsor",
-		"Monona",
-		"Waunakee",
-		"Madison",
-		"Sun Prairie",
-		"McFarland",
-		"Windsor",
-		"Monona",
-		"Waunakee",
-		"Madison",
-		"Sun Prairie",
-		"McFarland",
-		"Windsor",
-		"Monona",
-		"Waunakee",
-		"Madison",
-		"Sun Prairie",
-		"McFarland",
-		"Windsor",
-		"Monona",
-		"Waunakee",
-		"Madison",
-		"Sun Prairie",
-		"McFarland",
-		"Windsor",
-		"Monona",
-		"Waunakee",
-		"Madison",
-		"Sun Prairie",
-		"McFarland",
-		"Windsor",
-		"Monona",
-	}
-
-	pet := []string{
-		"Milo",
-		"Teddy",
-		"Buddy",
-		"Alfie",
-		"Max",
-		"Charlie",
-		"Bailey",
-		"Reggie",
-		"Bruno",
-		"Hugo",
-		"Luna",
-		"Bella",
-		"Lola",
-		"Poppy",
-		"Daisy",
-		"Coco",
-		"Ruby",
-		"Molly",
-		"Rosie",
-		"Willow",
-		"Milo",
-		"Teddy",
-		"Buddy",
-		"Alfie",
-		"Max",
-		"Charlie",
-		"Bailey",
-		"Reggie",
-		"Bruno",
-		"Hugo",
-		"Luna",
-		"Bella",
-		"Lola",
-		"Poppy",
-		"Daisy",
-		"Coco",
-		"Ruby",
-		"Molly",
-		"Rosie",
-		"Willow",
-	}
+	birth := "20" + strconv.Itoa(rand.Intn(15)+10) + "-" +
+		month + "-" + strconv.Itoa(rand.Intn(28)+1)
+	fmt.Println(birth)
 
 	rowner := Owner{
-		strconv.Itoa(rand.Intn(10) + 20),
+		id,
 		first[rand.Intn(len(first))],
 		last[rand.Intn(len(last))],
 		strconv.Itoa(rand.Intn(100)) + " " + strconv.Itoa(rand.Intn(len(street))),
@@ -346,7 +152,7 @@ func randData() (Owner, Pet) {
 		pet[rand.Intn(len(pet))],
 		"2006-10-10",
 		strconv.Itoa(rand.Intn(5) + 1),
-		strconv.Itoa(rand.Intn(10) + 20),
+		id,
 	}
 
 	return rowner, rpet
