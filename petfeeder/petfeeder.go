e245932281ab25b4328ca405f52e3eaf38947138
Jacint Juhasz <yyaazz@gmail.com>
2024-11-24 23:24:42 +0100

    first working release. packaged and running on K8s

--- petfeeder/petfeeder.go
+++ petfeeder/petfeeder.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"slices"
 	"strconv"
 	"time"
 
@@ -34,8 +35,8 @@ var pets []Pet
 var db *sql.DB
 
 var cfg = mysql.Config{
-	User:                 os.Getenv("DBUSER"),
-	Passwd:               os.Getenv("DBPASS"),
+	User:                 os.Getenv("PETDBUSER"),
+	Passwd:               os.Getenv("PETDBPASS"),
 	Net:                  "tcp",
 	Addr:                 "petclinic-db-mysql.petclinic:3306",
 	DBName:               "petclinic",
@@ -44,24 +45,38 @@ var cfg = mysql.Config{
 
 func main() {
 
-	// temporary
-	id := strconv.Itoa(rand.Intn(10) + 20)
-	// temporary
+	maxRows, err := strconv.Atoi(os.Getenv("PETLIMIT"))
+	if err != nil {
+		fmt.Println("PETLIMIT ENV is missing")
+	}
 
-	//	loadOwners()
-	//	loadPets()
-	a, b := randData(id, id)
+	interval, err := strconv.Atoi(os.Getenv("PETINT"))
+	if err != nil {
+		fmt.Println("PETINT ENV is missing")
+	}
 
-	fmt.Println(owners)
-	fmt.Println()
-	fmt.Println(pets)
-	fmt.Println()
-	fmt.Println(a)
-	fmt.Println(b)
+	for {
+
+		loadOwners()
+		loadPets()
+
+		if maxRows == 0 || len(owners) < maxRows {
+			soid, spid := getNextId()
+			rowner, rpet := randData(soid, spid)
+			insertData(rowner, rpet)
+			time.Sleep(time.Duration(interval) * time.Second)
+		} else {
+			deleteRows(len(owners))
+			time.Sleep(time.Duration(interval) * time.Second)
+		}
+	}
 
 }
 
 func loadOwners() {
+	owners = []Owner{}
+	pets = []Pet{}
+
 	db, err := sql.Open("mysql", cfg.FormatDSN())
 
 	if err != nil {
@@ -112,15 +127,105 @@ func loadPets() {
 	}
 }
 
-func insertOwners() {
+func insertData(rowner Owner, rpet Pet) (sql.Result, sql.Result) {
+
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+
+	if err != nil {
+		fmt.Println("SQL Open failed")
+	}
+
+	defer db.Close()
+
+	resultOwner, err := db.Exec("INSERT INTO owners VALUES (?, ?, ?, ?, ?, ?)",
+		rowner.Id, rowner.First, rowner.Last, rowner.Address, rowner.City, rowner.Phone)
+	if err != nil {
+		fmt.Println("Owner Insert Failed")
+	}
+
+	resultPet, err := db.Exec("INSERT INTO pets VALUES (?,?,?,?,?)",
+		rpet.Id, rpet.Name, rpet.Birth, rpet.Type, rpet.Oid)
+	if err != nil {
+		fmt.Println("Pet Insert Failed")
+	}
+
+	return resultOwner, resultPet
 
 }
 
-func getNextId(id string) {
+func getNextId() (string, string) {
+	var oid, pid int
+
+	maxOid, _ := strconv.Atoi(owners[len(owners)-1].Id)
+	if maxOid == len(owners) {
+		oid = maxOid
+	} else {
+		for _, v := range owners {
+			id, _ := strconv.Atoi(v.Id)
+			if id-oid == 1 {
+				oid++
+			}
+		}
+	}
+
+	maxPid, _ := strconv.Atoi(pets[len(pets)-1].Id)
+	if maxPid == len(pets) {
+		pid = maxPid
+	} else {
+		for _, v := range pets {
+			id, _ := strconv.Atoi(v.Id)
+			if id-pid == 1 {
+				pid++
+			}
+		}
+	}
+
+	finalOid := strconv.Itoa(oid + 1)
+	finalPid := strconv.Itoa(pid + 1)
+
+	return finalOid, finalPid
 
 }
 
-func randData(oid string, pid string) (Owner, Pet) {
+func deleteRows(row int) (sql.Result, sql.Result) {
+
+	var rows []int
+	var a, b sql.Result
+
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+
+	if err != nil {
+		fmt.Println("SQL Open failed")
+	}
+
+	defer db.Close()
+
+	for i := 0; i <= row/5; {
+		new := rand.Intn(row) + 1
+		if !slices.Contains(rows, new) && new != 6 {
+			rows = append(rows, new)
+		}
+		i = len(rows) + 1
+	}
+
+	for _, v := range rows {
+		a, err = db.Exec("DELETE FROM pets WHERE owner_id= (?)", v)
+
+		if err != nil {
+			fmt.Println("Pet Delete Failed")
+		}
+
+		b, err = db.Exec("DELETE FROM owners WHERE id = (?)", v)
+
+		if err != nil {
+			fmt.Println("Owner Insert Failed")
+		}
+
+	}
+	return a, b
+}
+
+func randData(soid string, spid string) (Owner, Pet) {
 
 	t := time.Now()
 	rand.NewSource(t.UnixNano())
@@ -128,6 +233,7 @@ func randData(oid string, pid string) (Owner, Pet) {
 	first := []string{"Jack", "Harry", "Jacob", "Charlie", "Thomas", "George", "Oscar", "James", "William", "Jake", "Connor", "Callum", "Jacob", "Kyle", "Joe", "Reece", "Rhys", "Charlie", "Damian", "Noah", "Liam", "Mason", "Jacob", "William", "Ethan", "Michael", "Alexander", "James", "Daniel", "James", "John", "Robert", "Michael", "William", "David", "Richard", "Joseph", "Charles", "Thomas", "Larry"}
 	last := []string{"Jones", "Williams", "Taylor", "Brown", "Davies", "Evans", "Wilson", "Thomas", "Johnson", "Roberts", "Robinson", "Thompson", "Wright", "Walker", "White", "Edwards", "Hughes", "Green", "Hall", "Lewis", "Harris", "Clarke", "Patel", "Jackson", "Wood", "Turner", "Martin", "Cooper", "Hill", "Ward", "Morris", "Moore", "Clark", "Lee", "King", "Baker", "Harrison", "Morgan", "Allen", "Johnson"}
 	street := []string{"Third", "First", "Fourth", "Park", "Fifth", "Main", "Sixth", "Oak", "Seventh", "Pine", "Maple", "Cedar", "Eighth", "Elm", "View", "Washington", "Ninth", "Lake", "Hill", "High", "Station", "Main", "Park", "Church", "London", "Victoria", "Green", "Manor", "Park", "Queens", "Grange", "Kings", "Kingsway", "Windsor", "Alexander", "York", "Broadway", "Springfield", "Richmond", "Taylor"}
+	stType := []string{"St.", "Rd.", "Dr.", "Av.", "Exp."}
 	city := []string{"Sun Prairie", "McFarland", "Windsor", "Monona", "Waunakee", "Madison", "Sun Prairie", "McFarland", "Windsor", "Monona", "Waunakee", "Madison", "Sun Prairie", "McFarland", "Windsor", "Monona", "Waunakee", "Madison", "Sun Prairie", "McFarland", "Windsor", "Monona", "Waunakee", "Madison", "Sun Prairie", "McFarland", "Windsor", "Monona", "Waunakee", "Madison", "Sun Prairie", "McFarland", "Windsor", "Monona", "Waunakee", "Madison", "Sun Prairie", "McFarland", "Windsor", "Monona"}
 	pet := []string{"Milo", "Teddy", "Buddy", "Alfie", "Max", "Charlie", "Bailey", "Reggie", "Bruno", "Hugo", "Luna", "Bella", "Lola", "Poppy", "Daisy", "Coco", "Ruby", "Molly", "Rosie", "Willow", "Milo", "Teddy", "Buddy", "Alfie", "Max", "Charlie", "Bailey", "Reggie", "Bruno", "Hugo", "Luna", "Bella", "Lola", "Poppy", "Daisy", "Coco", "Ruby", "Molly", "Rosie", "Willow"}
 
@@ -159,20 +265,20 @@ func randData(oid string, pid string) (Owner, Pet) {
 	birth := "20" + strconv.Itoa(rand.Intn(12)+year-2012) + "-" + month + "-" + day
 
 	rowner := Owner{
-		oid,
+		soid,
 		first[rand.Intn(len(first))],
 		last[rand.Intn(len(last))],
-		strconv.Itoa(rand.Intn(100)) + " " + strconv.Itoa(rand.Intn(len(street))),
+		strconv.Itoa(rand.Intn(5000)) + " " + street[rand.Intn(len(street))] + " " + stType[rand.Intn(len(stType))],
 		city[rand.Intn(len(city))],
-		strconv.Itoa(rand.Intn(1000000) + 1000000),
+		"6" + strconv.Itoa(rand.Intn(999999999)),
 	}
 
 	rpet := Pet{
-		pid,
+		spid,
 		pet[rand.Intn(len(pet))],
 		birth,
 		strconv.Itoa(rand.Intn(5) + 1),
-		oid,
+		soid,
 	}
 
 	return rowner, rpet
