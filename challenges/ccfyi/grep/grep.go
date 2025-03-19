1e235a0b9058c562f8f747a5d6fc3207f1936cd7
Jacint Juhasz <yyaazz@gmail.com>
2025-03-19 22:48:54 +0100

    Lazy commit Wed Mar 19 22:48:54 CET 2025

--- challenges/ccfyi/grep/grep.go
+++ challenges/ccfyi/grep/grep.go
@@ -13,50 +13,54 @@ var output []string
 func main() {
 
 	alen := len(os.Args)
-	var sub string
 
 	if alen == 1 {
-		// error
+		// usage
+		fmt.Println("usage: grep [-ivc]")
 	} else if alen == 2 {
 		// grep string stdin
 		readstdin()
-		sub = os.Args[1]
-		noarg(sub)
+		noarg(os.Args[1])
 		printout()
 	} else if alen == 3 {
 		if os.Args[1] == "-i" {
 			// grep -i string stdin
 			readstdin()
-			sub = os.Args[2]
-			argi(sub)
+			argi(os.Args[2])
 			printout()
 		} else if os.Args[1] == "-v" {
 			// grep -v string stdin
 			readstdin()
-			sub = os.Args[2]
-			argv(sub)
+			argv(os.Args[2])
 			printout()
+		} else if os.Args[1] == "-c" {
+			// grep -c string stdin
+			readstdin()
+			noarg(os.Args[2])
+			fmt.Println(len(output))
 		} else {
 			// grep string file
 			readfile(2)
-			sub = os.Args[1]
-			noarg(sub)
+			noarg(os.Args[1])
 			printout()
 		}
 	} else if alen == 4 {
 		// grep -i string file
-		if os.Args[1] == "i" {
+		if os.Args[1] == "-i" {
 			readfile(3)
-			sub = os.Args[2]
-			fmt.Println(sub) // <--- DOESNT WORK HERE!!!!
-			argi(sub)
+			argi(os.Args[2])
 			printout()
-		} else if os.Args[1] == "v" {
-
-		} else if os.Args[1] == "c" {
-
+		} else if os.Args[1] == "-v" {
+			readfile(3)
+			argv(os.Args[2])
+			printout()
+		} else if os.Args[1] == "-c" {
+			readfile(3)
+			noarg(os.Args[2])
+			fmt.Println(len(output))
 		} else {
-			//something error
+			//usage error
+			fmt.Printf("%v: invalid option -- %v\n", os.Args[0], os.Args[1])
 		}
 	}
 }
@@ -71,7 +75,7 @@ func readstdin() {
 func readfile(f int) {
 	in, err := os.ReadFile(os.Args[f])
 	if err != nil {
-		fmt.Printf("Error comes here\n")
+		fmt.Printf("%v: %v: No such file or directory\n", os.Args[0], os.Args[f])
 	}
 	input = strings.Split(string(in), "\n")
 }
@@ -106,8 +110,4 @@ func argv(s string) {
 			output = append(output, v)
 		}
 	}
-}
-
-func argc() {
-
 }
