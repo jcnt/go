9f22052226e2dca119f0b88fc0a064ec2ebb2260
Jacint Juhasz <yyaazz@gmail.com>
2026-04-08 13:27:06 +0200

    Advent of Code, 2025 day06, part 1 final working version

--- challenges/advent-of-code-2025/day06/part1.go
+++ challenges/advent-of-code-2025/day06/part1.go
@@ -5,14 +5,16 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
 
 	var source [][]string
+	var final int
 
-	in, err := os.ReadFile("example")
+	in, err := os.ReadFile("input")
 	if err != nil {
 		fmt.Println("unable to open file")
 	}
@@ -30,7 +32,33 @@ func main() {
 		source = append(source, l)
 	}
 
-	for _, v := range source {
-		fmt.Println(v)
+	for i := range source[0] {
+		var mlist []int
+		var s int
+		op := source[len(source)-1][i]
+		for _, v := range source[0 : len(source)-1] {
+			a, _ := strconv.Atoi(v[i])
+			mlist = append(mlist, a)
+		}
+		s = do_math(mlist, op)
+		final += s
 	}
+
+	fmt.Println("final answer is", final)
+}
+
+func do_math(l []int, o string) int {
+	var sum int
+	for _, v := range l {
+		if o == "+" {
+			sum += v
+		} else if o == "*" {
+			if sum == 0 {
+				sum += v
+			} else {
+				sum *= v
+			}
+		}
+	}
+	return sum
 }
