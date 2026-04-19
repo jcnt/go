171858017d489426ce7a46116850a6013391831d
Jacint Juhasz <yyaazz@gmail.com>
2026-04-19 20:00:53 +0200

    Advent of Code, 2025 day08, part 2 final working version

--- challenges/advent-of-code-2025/day08/part2.go
+++ challenges/advent-of-code-2025/day08/part2.go
@@ -1,4 +1,4 @@
-// Advent of Code, 2025. Day 8, part 1.
+// Advent of Code, 2025. Day 8, part 2.
 
 package main
 
@@ -23,8 +23,9 @@ func main() {
 	dist_db := []dist{}
 	conn := [][]int{}
 	boxlist := []int{}
-	answer := 1
 	slen := []int{}
+	changed := true
+	lastrun := []int{}
 
 	in, err := os.ReadFile("input")
 	if err != nil {
@@ -47,9 +48,12 @@ func main() {
 	conn = append(conn, []int{dist_db[0].a, dist_db[0].b})
 	boxlist = append(boxlist, dist_db[0].a, dist_db[0].b)
 
-	for i := 1; i < 1000; i++ {
-		//fmt.Println("current run is: ", dist_db[i])
+	i := 1
+	for changed {
+		changed = false
+		//fmt.Println("current run is: ", i, dist_db[i])
 		if slices.Contains(boxlist, dist_db[i].a) && slices.Contains(boxlist, dist_db[i].b) {
+			// if we have both we need to merge them
 			var a, b int
 			for j := range conn {
 				if slices.Contains(conn[j], dist_db[i].a) {
@@ -63,30 +67,55 @@ func main() {
 				conn[b] = append(conn[b], conn[a]...)
 				conn = slices.Delete(conn, a, a+1)
 				//fmt.Println(conn)
+				changed = true
 			} else if b > a {
 				conn[a] = append(conn[a], conn[b]...)
 				conn = slices.Delete(conn, b, b+1)
+				changed = true
 				//fmt.Println(conn)
+			} else if b == a && len(conn) > 1 {
+				changed = true
 			}
 		} else if slices.Contains(boxlist, dist_db[i].a) || slices.Contains(boxlist, dist_db[i].b) {
+			// if we have one we need to add the other
 			for j := range conn {
 				//fmt.Println("boxlist is: ", boxlist)
 				if slices.Contains(conn[j], dist_db[i].a) && slices.Contains(conn[j], dist_db[i].b) == false {
 					conn[j] = append(conn[j], dist_db[i].b)
 					boxlist = append(boxlist, dist_db[i].b)
+					if len(boxlist) == len(input) {
+						//fmt.Println("WE ARE DONE")
+						lastrun = append(lastrun, dist_db[i].a, dist_db[i].b)
+					}
+					changed = true
 					//fmt.Println("boxlist is: ", boxlist)
 				} else if slices.Contains(conn[j], dist_db[i].b) && slices.Contains(conn[j], dist_db[i].a) == false {
 					conn[j] = append(conn[j], dist_db[i].a)
 					boxlist = append(boxlist, dist_db[i].a)
+					if len(boxlist) == len(input) {
+						//fmt.Println("WE ARE DONE")
+						lastrun = append(lastrun, dist_db[i].a, dist_db[i].b)
+					}
+					changed = true
 					//fmt.Println("boxlist is: ", boxlist)
 				}
 			}
 		} else {
+			// if we have none, we need to add both into a new junction
 			conn = append(conn, []int{dist_db[i].a, dist_db[i].b})
 			boxlist = append(boxlist, dist_db[i].a, dist_db[i].b)
+			if len(boxlist) == len(input) {
+				//fmt.Println("WE ARE DONE")
+				lastrun = append(lastrun, dist_db[i].a, dist_db[i].b)
+			}
+			changed = true
 			//fmt.Println("boxlist is: ", boxlist)
 		}
 		//fmt.Println("conn is: ", conn)
+		i++
+		if len(boxlist) < len(input) {
+			changed = true
+		}
 	}
 
 	for _, v := range conn {
@@ -94,10 +123,19 @@ func main() {
 	}
 	sort.Slice(slen, func(i, j int) bool { return slen[i] > slen[j] })
 
-	for i := 0; i < 3; i++ {
-		answer *= slen[i]
-	}
-	fmt.Println("Answer is: ", answer)
+	fmt.Println(conn)
+	fmt.Println(len(input))
+	fmt.Println(i)
+	fmt.Println(lastrun)
+	fmt.Println(answer(input[lastrun[0]], input[lastrun[1]]))
+}
+
+func answer(s1, s2 string) int {
+	la := strings.Split(s1, ",")
+	lb := strings.Split(s2, ",")
+	a, _ := strconv.Atoi(la[0])
+	b, _ := strconv.Atoi(lb[0])
+	return a * b
 }
 
 func get_distance(a string, b string) float64 {
