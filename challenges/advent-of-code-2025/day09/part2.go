733bf930cde3b0994b6bcd6c1fb7ec79cbee19c5
Jacint Juhasz <yyaazz@gmail.com>
2026-04-24 22:49:58 +0200

    Lazy commit Fri Apr 24 22:49:58 CEST 2026

--- challenges/advent-of-code-2025/day09/part2.go
+++ challenges/advent-of-code-2025/day09/part2.go
@@ -19,7 +19,7 @@ func main() {
 	}
 	input := strings.Split(string(in), "\n")
 	input = input[0 : len(input)-1]
-	fmt.Printf("%q\n", input)
+	//fmt.Printf("%q\n", input)
 
 	for _, v := range input {
 		intred := []int{}
@@ -48,6 +48,27 @@ func main() {
 	for _, t := range reddb {
 		tiledb[t[1]][t[0]] = "#"
 	}
+
+	for i := 1; i < len(reddb); i++ {
+		c0 := reddb[i-1][0]
+		c1 := reddb[i-1][1]
+		n0 := reddb[i][0]
+		n1 := reddb[i][1]
+		if c0 == n1 {
+			if c1 < n1 {
+				fmt.Println(c1, n1)
+			} else {
+				fmt.Println(n1, c1)
+			}
+		} else {
+			if c0 < n0 {
+				fmt.Println(c0, n0)
+			} else {
+				fmt.Println(n0, c0)
+			}
+		}
+	}
+
 	for _, v := range tiledb {
 		fmt.Printf("%q\n", v)
 	}
