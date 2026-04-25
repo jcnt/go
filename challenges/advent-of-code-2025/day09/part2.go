77fe24ba64f2bdc2e48334db86f716a4eb205ccf
Jacint Juhasz <yyaazz@gmail.com>
2026-04-25 20:40:15 +0200

    Lazy commit Sat Apr 25 20:40:15 CEST 2026

--- challenges/advent-of-code-2025/day09/part2.go
+++ challenges/advent-of-code-2025/day09/part2.go
@@ -11,6 +11,7 @@ import (
 
 func main() {
 	reddb := [][]int{}
+	rgdb := [][]int{}
 	tiledb := [][]string{}
 	var maxx, maxy int
 	in, err := os.ReadFile("example")
@@ -44,29 +45,43 @@ func main() {
 		}
 		tiledb = append(tiledb, tline)
 	}
-
-	for _, t := range reddb {
-		tiledb[t[1]][t[0]] = "#"
-	}
+	reddb = append(reddb, reddb[0])
 
 	for i := 1; i < len(reddb); i++ {
 		c0 := reddb[i-1][0]
 		c1 := reddb[i-1][1]
 		n0 := reddb[i][0]
 		n1 := reddb[i][1]
-		if c0 == n1 {
+		if c0 == n0 {
 			if c1 < n1 {
-				fmt.Println(c1, n1)
+				for j := c1 + 1; j < n1; j++ {
+					rgdb = append(rgdb, []int{c0, j})
+				}
 			} else {
-				fmt.Println(n1, c1)
+				for j := n1 + 1; j < c1; j++ {
+					rgdb = append(rgdb, []int{c0, j})
+				}
 			}
 		} else {
 			if c0 < n0 {
-				fmt.Println(c0, n0)
+				for j := c0 + 1; j < n0; j++ {
+					rgdb = append(rgdb, []int{j, c1})
+				}
 			} else {
-				fmt.Println(n0, c0)
+				for j := n0 + 1; j < c0; j++ {
+					rgdb = append(rgdb, []int{j, c1})
+				}
 			}
 		}
+	}
+
+	fmt.Println(reddb)
+	fmt.Println(rgdb)
+	for _, t := range reddb {
+		tiledb[t[1]][t[0]] = "#"
+	}
+	for _, t := range rgdb {
+		tiledb[t[1]][t[0]] = "#"
 	}
 
 	for _, v := range tiledb {
