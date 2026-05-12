e63cc7f633ad81eea80951b21405c83ae3856a3f
Jacint Juhasz <yyaazz@gmail.com>
2026-05-12 11:35:54 +0300

    Lazy commit Tue May 12 11:35:54 +03 2026

--- challenges/advent-of-code-2024/day02/part2.go
+++ challenges/advent-of-code-2024/day02/part2.go
@@ -21,11 +21,10 @@ func main() {
 	input := strings.Split(string(in), "\n")
 	input = input[:len(input)-1]
 
-	reactor := make([][]int, 0, len(input))
-	second := make([][]int, 0, len(input))
+	var reactor, second [][]int
 	for _, v := range input {
 		sti := strings.Split(v, " ")
-		t := make([]int, 0, len(sti))
+		t := []int{}
 		for _, w := range sti {
 			a, _ := strconv.Atoi(w)
 			t = append(t, a)
@@ -35,56 +34,55 @@ func main() {
 
 	for _, line := range reactor {
 		safe := 0
-		if line[0] > line[len(line)-1] {
-			for i := 0; i < len(line)-1; i++ {
-				if line[i]-line[i+1] > 0 && line[i]-line[i+1] < 4 {
-					safe += 1
-				} else {
-					second = append(second, slices.Delete(line, i+1, i+2))
-					break
-				}
-			}
-			if safe == len(line)-1 {
-				sum += 1
-			}
-		} else if line[0] < line[len(line)-1] {
-			for i := 0; i < len(line)-1; i++ {
-				if line[i+1]-line[i] > 0 && line[i+1]-line[i] < 4 {
-					safe += 1
-				} else {
-					second = append(second, slices.Delete(line, i+1, i+2))
-					break
-				}
-			}
-			if safe == len(line)-1 {
-				sum += 1
+		for i := 0; i < len(line)-1; i++ {
+			if line[i]-line[i+1] > 0 && line[i]-line[i+1] < 4 {
+				safe += 1
+			} else {
+				tl := slices.Delete(line, i+1, i+2)
+				second = append(second, tl)
+				break
 			}
 		}
+		if safe == len(line)-1 {
+			sum += 1
+		}
+		safe = 0
+		for i := 0; i < len(line)-1; i++ {
+			if line[i+1]-line[i] > 0 && line[i+1]-line[i] < 4 {
+				safe += 1
+			} else {
+				tl := slices.Delete(line, i+1, i+2)
+				second = append(second, tl)
+				break
+			}
+		}
+		if safe == len(line)-1 {
+			sum += 1
+		}
 	}
+
 	for _, line := range second {
 		safe := 0
-		if line[0] > line[len(line)-1] {
-			for i := 0; i < len(line)-1; i++ {
-				if line[i]-line[i+1] > 0 && line[i]-line[i+1] < 4 {
-					safe += 1
-				} else {
-					break
-				}
+		for i := 0; i < len(line)-1; i++ {
+			if line[i]-line[i+1] > 0 && line[i]-line[i+1] < 4 {
+				safe += 1
+			} else {
+				break
 			}
-			if safe == len(line)-1 {
-				sum += 1
+		}
+		if safe == len(line)-1 {
+			sum += 1
+		}
+		safe = 0
+		for i := 0; i < len(line)-1; i++ {
+			if line[i+1]-line[i] > 0 && line[i+1]-line[i] < 4 {
+				safe += 1
+			} else {
+				break
 			}
-		} else if line[0] < line[len(line)-1] {
-			for i := 0; i < len(line)-1; i++ {
-				if line[i+1]-line[i] > 0 && line[i+1]-line[i] < 4 {
-					safe += 1
-				} else {
-					break
-				}
-			}
-			if safe == len(line)-1 {
-				sum += 1
-			}
+		}
+		if safe == len(line)-1 {
+			sum += 1
 		}
 	}
 	fmt.Println("sum is ", sum)
